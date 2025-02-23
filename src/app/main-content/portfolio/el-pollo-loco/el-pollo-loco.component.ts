3b8a6ed5a97001356b91bf583c332a437c99e4bb
seidenberger <158757732+seidenberger@users.noreply.github.com>
2025-02-23 07:23:44 +0100

    first commit

---
+++ src/app/main-content/portfolio/el-pollo-loco/el-pollo-loco.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-el-pollo-loco',
+  imports: [],
+  templateUrl: './el-pollo-loco.component.html',
+  styleUrl: './el-pollo-loco.component.scss'
+})
+export class ElPolloLocoComponent {
+
+}
