1f1773fcd7a50963bb2c5bc326a792a1043d41d1
seidenberger <158757732+seidenberger@users.noreply.github.com>
2025-05-14 09:59:10 +0200

    line

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+iconsLandingpage: string[] = [
+  'assets/icons/Property 1=Default.png',
+ 'assets/icons/Property 1=Default (1).png',
+ 'assets/icons/Property 1=Default (2).png',
 
+]
 }
