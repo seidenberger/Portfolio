e656b9137ff94bd88c766f65631f67fc2de4411a
seidenberger <158757732+seidenberger@users.noreply.github.com>
2025-05-31 21:06:29 +0200

    fix: resolve bug from header for responsive in footer

--- src/app/main-content/description-of-other/description-jaso.ts
+++ src/app/main-content/description-of-other/description-jaso.ts
@@ -2,12 +2,12 @@ export const carousel =   [
     {
      "image": 'assets/img/description/übung/IMG_0547.JPG', 
     
-      "text": "test 1 lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia expedita vero doloremque dolorem voluptatem nisi nobis voluptas ipsa vitae repellat, ipsum harum unde assumenda odit. Nesciunt soluta voluptatem voluptate cum. ",
+      "text": "test 1 lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia expedita vero doloremque dolorem voluptatem nisi ",
         "signature": "max team Partner"
     },
     {
         "image": 'assets/img/description/übung/WIN_20250223_21_13_37_Pro.jpg',
-        "text": "2 test 2 ",
+        "text": "2 test 2   nobis voluptas ipsa vitae repellat, ipsum harum unde assumenda odit. Nesciunt soluta voluptatem voluptate cum.",
         "signature": "musterman team Partner"
     },
     {
