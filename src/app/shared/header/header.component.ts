f19e0c0683118111ed7e6b1b0f0350d4d906f2a4
seidenberger <158757732+seidenberger@users.noreply.github.com>
2025-06-13 16:14:37 +0200

    feat: updated nav a header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,48 +1,53 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { TranslationService } from '../../translation.service';
-import { Subscription } from 'rxjs';
+import { Component} from '@angular/core';
+// import { TranslationService } from '../../translation.service';
+// import { Subscription } from 'rxjs';
+import { MenuOverlayComponent } from './menu-overlay/menu-overlay.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MenuOverlayComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements OnInit, OnDestroy {
-    
-      about= '';
-      skill= '';
-      portfolio= '';
-      private sub!: Subscription;
+export class HeaderComponent {
+  showMenu = false;  
 
-  constructor(public translationService: TranslationService) {}
+  //     about= '';
+  //     skill= '';
+  //     portfolio= '';
+  //     private sub!: Subscription;
+
+  // constructor(public translationService: TranslationService) {}
 
   // switchLang(lang: 'de' | 'en') {
   //   this.translationService.setLanguage(lang);
   //   console.log(lang)
   // }
+  toggelMenu() {
+  this.showMenu = !this.showMenu;
+}
 
-  ngOnInit(): void {
-    this.updateTexts(); // Initiale Texte
-    this.sub = this.translationService.lang$.subscribe(() => {
-      this.updateTexts(); // Reagiere auf Sprachwechsel
-    });
-  }
+  // ngOnInit(): void {
+  //   this.updateTexts(); // Initiale Texte
+  //   this.sub = this.translationService.lang$.subscribe(() => {
+  //     this.updateTexts(); // Reagiere auf Sprachwechsel
+  //   });
+  // }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe(); // Speicher aufräumen
-  }
+  // ngOnDestroy(): void {
+  //   this.sub.unsubscribe(); // Speicher aufräumen
+  // }
 
-  updateTexts() {
-    // debugger
-    this.about = this.translationService.translate('about');
-    this.skill = this.translationService.translate('skill');
-    this.portfolio = this.translationService.translate('portfolio');
-  }
+  // updateTexts() {
+  //   // debugger
+  //   this.about = this.translationService.translate('about');
+  //   this.skill = this.translationService.translate('skill');
+  //   this.portfolio = this.translationService.translate('portfolio');
+  // }
 
-  switchLang(lang: 'de' | 'en') {
-    this.translationService.setLanguage(lang);
-    console.log(lang);
-  }
+  // switchLang(lang: 'de' | 'en') {
+  //   this.translationService.setLanguage(lang);
+  //   console.log(lang);
+  // }
 
 }
--- src/app/shared/header/menu-overlay/menu-overlay.component.ts
+++ src/app/shared/header/menu-overlay/menu-overlay.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TranslationService } from '../../../translation.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-menu-overlay',
@@ -6,6 +8,37 @@ import { Component } from '@angular/core';
   templateUrl: './menu-overlay.component.html',
   styleUrl: './menu-overlay.component.scss'
 })
-export class MenuOverlayComponent {
+export class MenuOverlayComponent  implements OnInit, OnDestroy{
 
+
+      about= '';
+      skill= '';
+      portfolio= '';
+      private sub!: Subscription;
+
+  constructor(public translationService: TranslationService) {}
+
+
+  ngOnInit(): void {
+    this.updateTexts(); // Initiale Texte
+    this.sub = this.translationService.lang$.subscribe(() => {
+      this.updateTexts(); // Reagiere auf Sprachwechsel
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe(); // Speicher aufräumen
+  }
+
+  updateTexts() {
+    // debugger
+    this.about = this.translationService.translate('about');
+    this.skill = this.translationService.translate('skill');
+    this.portfolio = this.translationService.translate('portfolio');
+  }
+
+  switchLang(lang: 'de' | 'en') {
+    this.translationService.setLanguage(lang);
+    console.log(lang);
+  }
 }
