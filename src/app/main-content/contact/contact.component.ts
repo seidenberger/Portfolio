1725e8a5de03c6e3e3ca1efd0df90c68d9152386
seidenberger <158757732+seidenberger@users.noreply.github.com>
2025-06-24 21:10:12 +0200

    feat: updated contact form

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 import { FormsModule, NgForm } from '@angular/forms'; 
 
@@ -20,15 +21,79 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class ContactComponent {
 
+  http = inject(HttpClient)
+
+  showPrivacyError = false; 
 
   contactData = {
     name: "",
     email: "",
     message: "",
+     privacyAccepted: false
   }
 
+//   onSubmit(ngForm: NgForm) {
+//     if(ngForm.valid && ngForm.submitted){
+//     console.log(this.contactData)
+//   }
+// }
+
+  
+
+
+
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'http://https://theodor-seidenberger.de///sendMail.php',
+    // endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
-    if(ngForm.valid && ngForm.submitted){
-    console.log(this.contactData)
-  }}
+     console.log(this.contactData);
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
+
+
+
+
+//   onSubmit(form: NgForm) {
+//   console.log("läuft");
+//   console.log(this.contactData)
+
+//   console.log(this.showPrivacyError)
+//   if (!this.contactData.privacyAccepted) {
+//     this.showPrivacyError = true;
+//     return; // breche ab
+//   } 
+
+//   if (form.valid) {
+//     this.showPrivacyError = false;
+//     console.log('Formular erfolgreich gesendet', this.contactData);
+//     // Weiterer Code hier ...
+//   }
+// }
 } 
