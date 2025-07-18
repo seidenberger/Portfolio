eb8c6050fc81c8560cb5ae84306821ee280095c7
seidenberger <158757732+seidenberger@users.noreply.github.com>
2025-07-18 18:29:53 +0200

    fix portfolio Translation

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { CommonModule, NgIf } from '@angular/common';
-import { portfolio } from './portfolio-json';
-import { translations } from '../../translations';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TranslationService } from '../../translation.service';
-import { Subscription } from 'rxjs';
 import { JoinComponent } from './join/join.component';
 import { ElPolloLocoComponent } from './el-pollo-loco/el-pollo-loco.component';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { BreakpointObserver} from '@angular/cdk/layout';
+import { BaseTranslatedComponent } from '../../base-translated.component';
 
 @Component({
   selector: 'app-portfolio',
@@ -14,73 +12,31 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
-export class PortfolioComponent  {
+export class PortfolioComponent extends BaseTranslatedComponent  {
 
 
   reverse = true;
 
 
-    constructor(private breakpointObserver: BreakpointObserver) {
+    constructor(
+      private breakpointObserver: BreakpointObserver,
+    public override translationService: TranslationService
+    ) {
+       super(translationService);
+
     this.breakpointObserver.observe(['(max-width: 1200px)']).subscribe(result => {
       this.reverse = !result.matches; 
           console.log('Reverse / breakpoint:', this.reverse);
     });
 
+
+
   }
 
-  // constructor(private breakpointObserver: BreakpointObserver) {}
-
-  // ngOnInit() {
-  //   this.breakpointObserver
-  //     .observe(['(max-width: 900px)'])
-  //     .subscribe(result => {
-  //       this.reverse = !result.matches;
-  //     });
-  // }
-
-
-  // portfolio = portfolio;
-
   descriptionPortfoli= '';
-  // join_description= '';
-  // pollo_loco_description= '';
 
-  // currentLang: string = 'de';
+    protected override updateTexts() {
+    this.descriptionPortfoli = this.translationService.translate('descriptionPortfoli');
+    }
 
-  // private sub!: Subscription;
-  
-  // constructor(public translationService: TranslationService) {}
-
-  // ngOnInit(): void {
-  //   this.updateTexts(); // Initiale Texte
-  //   this.sub = this.translationService.lang$.subscribe(() => {
-  //     this.updateTexts(); // Reagiere auf Sprachwechsel
-  //   });
-  // }
-
-  
-  
-  // ngOnDestroy(): void {
-  //   this.sub.unsubscribe(); // Speicher aufräumen
-  // }
-  
-  // updateTexts() {
-  //   // debugger
-  //    this.descriptionPortfoli = this.translationService.translate('descriptionPortfoli');
-  //   this.join_description = this.translationService.translate('join_description');
-  //   this.pollo_loco_description = this.translationService.translate('pollo_loco_description');
-   
-    
-  // }
-
-  
-  
-  // switchLang(lang: 'de' | 'en') {
-  //   this.translationService.setLanguage(lang);
-  //   console.log(lang);
-  // }
-
-  // getTranslation(key: string, lang: string): string {
-  //   return translations[lang][key] || '';
-  // }
 }
