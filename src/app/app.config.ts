85cafe79c1457db20c722d31cfad6533a37652d8
seidenberger <158757732+seidenberger@users.noreply.github.com>
2026-01-23 22:28:01 +0100

    refactor: routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -7,4 +7,10 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient(), provideZoneChangeDetection({ eventCoalescing: true })
   ]
+
+  
 };
+
+// provideRouter(routes, withInMemoryScrolling({
+//   anchorScrolling: 'enabled',
+// }))
