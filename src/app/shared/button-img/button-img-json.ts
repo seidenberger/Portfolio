7b3581b9d78d88c4cdc95d39065553a5d15e5860
seidenberger <158757732+seidenberger@users.noreply.github.com>
2026-03-10 22:41:07 +0100

    refactor: hedaer and landin. change img

--- src/app/shared/button-img/button-img-json.ts
+++ src/app/shared/button-img/button-img-json.ts
@@ -2,18 +2,18 @@ export const buttonImgs = [
 
         {
         "link": 'https://www.github.com/seidenberger',
-        "image": 'assets/icons/github.png',
+        "image": 'assets/icons/github-default.svg',
         external: true
     },
 
     {
         "link": 'https://www.linkedin.com/feed/?trk=guest_homepage-basic_nav-header-signin',
-        "image": 'assets/icons/linkedin.png',
+        "image": 'assets/icons/linkedin-default.svg',
         external: true
     },
         {
         "link": '#contact',
-        "image": 'assets/icons/email.png',
+        "image": 'assets/icons/email-default.svg',
         external: false
     },
 ]
