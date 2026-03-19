e9e676dcfecae83261b78a264ed38e0e8c690186
seidenberger <158757732+seidenberger@users.noreply.github.com>
2026-03-19 07:06:17 +0100

    refactor: skills img

--- src/app/main-content/my-skills/my-skills-json.ts
+++ src/app/main-content/my-skills/my-skills-json.ts
@@ -1,57 +1,58 @@
 export const mySkills =   [
     {
-      "image": 'assets/img/skills/html.png',
+      "image": 'assets/img/skills/html.svg',
       "text": "HTML",
     active: true
     },
     {
-        "image": 'assets/img/skills/css.png',
+        "image": 'assets/img/skills/css.svg',
         "text": "CSS",
         active: true
     },
     {
-        "image": 'assets/img/skills/JavScript.png',
+        "image": 'assets/img/skills/JavScript.svg',
         "text": "JavaScript",
      active: true
     },
     {
-        "image": 'assets/img/skills/typescript.png',
+        "image": 'assets/img/skills/typescript.svg',
         "text": "TypeScript",
      active: true
     },
     {
-        "image": 'assets/img/skills/angular.png',
+        "image": 'assets/img/skills/angular.svg',
         "text": "Angular",
      active: true
     },
     {
-        "image": 'assets/img/skills/firebase.png',
+        "image": 'assets/img/skills/firebase.svg',
         "text": "Firebase",
      active: true
     },
     {
-        "image": 'assets/img/skills/git.png',
+        "image": 'assets/img/skills/git.svg',
         "text": "Git",
      active: true
     },
     {
-        "image": 'assets/img/skills/api.png',
+        "image": 'assets/img/skills/api.svg',
         "text": "REST API",
      active: true
     },
     {
-        "image": 'assets/img/skills/scrum.png',
+        "image": 'assets/img/skills/scrum.svg',
         "text": "Scrum",
      active: true
     },
     {
-        "image": 'assets/img/skills/material-design.png',
+        "image": 'assets/img/skills/material-design.svg',
         "text": "Material  Design",
      active: true
     },
         {
-            "imagew": 'assets/img/skills/continually-learning.png',
-        "image": 'assets/img/skills/learning.png',
+            "imageW": 'assets/img/skills/continually-learning.svg',
+            // "imagew": 'assets/img/skillssssss/continually-learning.png',
+        "image": 'assets/img/skills/learning.svg',
         "text": "Continually learning",
      active: false
     } 
